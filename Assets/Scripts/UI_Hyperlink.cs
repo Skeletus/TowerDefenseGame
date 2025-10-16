a2be78117a7abda87daf0b3a419da899d0157811
Skeletus <Samol_88@hotmail.com>
2025-10-16 16:54:24 -0500

    Added Hyperlink to creditUI

---
+++ Assets/Scripts/UI_Hyperlink.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Hyperlink : MonoBehaviour
+{
+    [SerializeField] private string url;
+
+    public void OpenUrl()
+    {
+        Application.OpenURL(url);
+    }
+}
